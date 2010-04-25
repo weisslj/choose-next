1cbc932559ed802e5f2639dd451b9416c813fb51
Johannes Weißl <jargon@molb.org>
2010-04-26 00:02:39 +0200

    version 1.0

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='choose-next',
+      version='1.0',
+      description='Chooses a file from a directory. '\
+                  'Very handy to re-watch tv series!',
+      author='Johannes Weißl',
+      author_email='jargon@molb.org',
+      url='http://github.com/weisslj/choose-next',
+      scripts=['choose-next'])
