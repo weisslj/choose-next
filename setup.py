05ef509b5e3ddaf85046d270e6079294989fdc5f
Johannes Weißl <jargon@molb.org>
2017-03-14 23:01:31 +0100

    Cosmetics

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+"""A setuptools based setup module."""
 
 from distutils.core import setup
 
-setup(name='choose-next',
-      version='1.1',
-      description='Chooses a file from a directory. '\
-                  'Very handy to re-watch tv series!',
-      author=u'Johannes Weißl',
-      author_email='jargon@molb.org',
-      url='http://github.com/weisslj/choose-next',
-      license='GNU GPL v3',
-      classifiers=[
-          'License :: OSI Approved :: GNU General Public License (GPL)',
-          'Programming Language :: Python :: 2.5',
-          'Topic :: Utilities',
-      ],
-      scripts=['choose-next'])
+setup(
+    name='choose-next',
+    version='1.1',
+    description='Chooses a file from a directory. '\
+                'Very handy to re-watch tv series!',
+    author=u'Johannes Weißl',
+    author_email='jargon@molb.org',
+    url='http://github.com/weisslj/choose-next',
+    license='GNU GPL v3',
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python :: 2.5',
+        'Topic :: Utilities',
+    ],
+    scripts=['choose-next'],
+)
