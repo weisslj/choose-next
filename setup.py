60afec9ca59b9c3163c577697a4b2d4f98f6addd
Johannes Weißl <jargon@molb.org>
2017-04-30 17:46:42 +0200

    Cosmetics

--- setup.py
+++ setup.py
@@ -3,13 +3,19 @@
 """A setuptools based setup module."""
 
 from __future__ import unicode_literals
+import os
+import codecs
 from setuptools import setup
+
+# Get the long description from the README file
+HERE = os.path.abspath(os.path.dirname(__file__))
+with codecs.open(os.path.join(HERE, 'README.rst'), encoding='utf-8') as stream:
+    LONG_DESCRIPTION = stream.read()
 
 setup(
     name='choose-next',
     version='1.1',
-    description='Chooses a file from a directory. '\
-                'Very handy to re-watch tv series!',
+    description='Chooses a file from a directory. Very handy to re-watch tv series!',
     author='Johannes Weißl',
     author_email='jargon@molb.org',
     url='https://github.com/weisslj/choose-next',
